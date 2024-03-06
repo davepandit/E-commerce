a0230144f9833be62f61ebd35744a0bc8e4e9d46
Debajyoti Pandit <codeninja0812@gmail.com>
2024-03-07 00:25:53 +0530

    Products card added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext'
 import HeroBanner from '../../components/herobanner/HeroBanner'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/product/Product'
 
 const Home = () => {
   //establishing the link between the component and the state
@@ -10,6 +12,8 @@ const Home = () => {
     <>
       <div>
        <HeroBanner />
+       <Filter />
+       <ProductCard />
       </div>
     </>
   )
